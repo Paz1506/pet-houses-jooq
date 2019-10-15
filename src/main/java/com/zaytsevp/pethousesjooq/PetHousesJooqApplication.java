37fbe4ab546860e522ae8d45da1d8064c4009e6e
Zaytsev Pavel <zaytsev@speechpro.com>
2019-10-15 13:26:32 +0300

    init

---
+++ src/main/java/com/zaytsevp/pethousesjooq/PetHousesJooqApplication.java
@@ -0,0 +1,13 @@
+package com.zaytsevp.pethousesjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHousesJooqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetHousesJooqApplication.class, args);
+    }
+
+}
